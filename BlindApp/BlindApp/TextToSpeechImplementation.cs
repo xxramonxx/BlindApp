41c5598e5100704fc5918aad915c6f20c3ed6ee3
Roman Pikna <pik.ramon@gmail.com>
2016-10-01 11:18:39 +0200

    test

---
+++ BlindApp/BlindApp/TextToSpeechImplementation.cs
@@ -0,0 +1,38 @@
+﻿using Android.Speech.Tts;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using DependencyServiceSample.Droid;
+
+public class TextToSpeechImplementation : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+{
+    TextToSpeech speaker;
+    string toSpeak;
+
+    public TextToSpeechImplementation() { }
+
+    public void Speak(string text)
+    {
+        var ctx = Forms.Context; // useful for many Android SDK features
+        toSpeak = text;
+        if (speaker == null)
+        {
+            speaker = new TextToSpeech(ctx, this);
+        }
+        else
+        {
+            var p = new Dictionary<string, string>();
+            speaker.Speak(toSpeak, QueueMode.Flush, p);
+        }
+    }
+
+    #region IOnInitListener implementation
+    public void OnInit(OperationResult status)
+    {
+        if (status.Equals(OperationResult.Success))
+        {
+            var p = new Dictionary<string, string>();
+            speaker.Speak(toSpeak, QueueMode.Flush, p);
+        }
+    }
+    #endregion
+}
